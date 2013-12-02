29b1a6b091421d6b2727d32cf31160590fcb7c64
Viacheslav Blinov <sblinoff@gmail.com>
2013-12-02 20:33:16 +0000

    v2

--- web.js
+++ web.js
@@ -1,13 +1,10 @@
 var express = require('express');
 var app = express();
-var fs = require ('fs');
+
 
 app.use(express.logger());
-var buffer = fs.readFileSync ('index.html');
-var text= buffer.toSrtring('utf-8');
-
 app.get('/', function(request, response) {
-  response.send('text');
+  response.send('qecdwedwecftext');
 });
 
 var port = process.env.PORT || 5000;
